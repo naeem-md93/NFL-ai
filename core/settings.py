6d02b1a5e7488f64f336b4fb4d8a546cd9269aed
Naeem Mohammadi <naeem.mohammadi1993@gmail.com>
2025-10-10 07:28:37 +0330

    moved .env to Shared directory

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import socket
 from pathlib import Path
 
 
